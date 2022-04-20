6b413d80fca3a55447ff4999ecf542257aafa216
leonardoCatharin <leo_catharin@hotmail.com>
2022-04-19 22:24:07 -0300

    primeiro commit

---
+++ src/test/java/com/aulaspringboot/aulaspringboot/AulaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulaspringboot.aulaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
